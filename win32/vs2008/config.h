f64096b39a68c0983ec7697d93f326801571be16
Anders Lövgren <andlov@nowise.se>
2010-10-19 19:35:12 +0000

    Adding visual studio 2008 solution creating libinifile.dll and libinifile++.dll as targets.

---
+++ win32/vs2008/config.h
@@ -0,0 +1,40 @@
+/* libinifile - C/C++ library for parsing ini-style configuration files.
+ * Copyright (C) 2008-2010  Anders Lövgren, QNET/BMC Compdept, Uppsala University
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ---
+ * Send questions to: Anders Lövgren <lespaul@algonet.se>
+ */
+
+/*
+ * Common defines for Visual Studio 2008 C/C++ (VC9)
+ */
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define ssize_t to large integer */
+typedef long long ssize_t;
+
+/* Disable warnings, this is bug free code anyway ;-) */
+#define _CRT_SECURE_NO_WARNINGS 
+
+#endif  /* __CONFIG_H__ */
