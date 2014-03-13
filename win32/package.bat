d8436271d9d868b1dda7bb5581b53ddf41558736
Anders Lövgren <andlov@nowise.se>
2014-03-13 18:21:34 +0000

    Separate include from library in destination package.

--- win32/package.bat
+++ win32/package.bat
@@ -10,26 +10,30 @@ set zipcmd=C:\Program Files\7-Zip\7z.exe
 
 set libdir=vs2008\Release
 set srcdir=..\src
-set pkgdir=%pkgname%
+set pkgdir=%pkgname%-%pkgvers%
+
+set incdir=%pkgdir%\include
+set lib32=%pkgdir%\x86
+set lib64=%pkgdir%\x64
+
+:: headers:
+if not exist %incdir% mkdir %incdir%
+xcopy /y %libdir%\sample.conf %pkgdir%
+xcopy /y %srcdir%\inifile.h %incdir%
+xcopy /y %srcdir%\cxx\inifile++.hpp %incdir%
 
 :: 32-bit:
-if not exist %pkgdir% mkdir %pkgdir%
+if not exist %lib32% mkdir %lib32%
 
-xcopy /y %libdir%\*.dll %pkgdir%
-xcopy /y %libdir%\*.exp %pkgdir%
-xcopy /y %libdir%\*.lib %pkgdir%
-xcopy /y %libdir%\sample.conf %pkgdir%
-xcopy /y %srcdir%\inifile.h %pkgdir%
-xcopy /y %srcdir%\cxx\inifile++.hpp %pkgdir%
+xcopy /y %libdir%\*.dll %lib32%
+xcopy /y %libdir%\*.exp %lib32%
+xcopy /y %libdir%\*.lib %lib32%
 
 :: 64-bit:
-if not exist %pkgdir%\x64 mkdir %pkgdir%\x64
+if not exist %lib64% mkdir %lib64%
 
-xcopy /y %libdir%\*.dll %pkgdir%\x64
-xcopy /y %libdir%\*.exp %pkgdir%\x64
-xcopy /y %libdir%\*.lib %pkgdir%\x64
-xcopy /y %libdir%\sample.conf %pkgdir%\x64
-xcopy /y %srcdir%\inifile.h %pkgdir%\x64
-xcopy /y %srcdir%\cxx\inifile++.hpp %pkgdir%\x64
+xcopy /y %libdir%\*.dll %lib64%
+xcopy /y %libdir%\*.exp %lib64%
+xcopy /y %libdir%\*.lib %lib64%
 
 "%zipcmd%" a %pkgname%-%pkgvers%.zip %pkgdir%
