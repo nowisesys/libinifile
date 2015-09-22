b2a22cfc7f3deca398741041fe0790e1c0a1b814
Anders Lövgren <andlov@nowise.se>
2015-09-22 09:59:38 +0000

    Restructured build target. Packaging is now fixed to including correct x86/x64 dll-files.

--- win32/package.bat
+++ win32/package.bat
@@ -8,7 +8,6 @@ set pkgvers=0.6.1
 
 set zipcmd=C:\Program Files\7-Zip\7z.exe
 
-set libdir=vs2008\Release
 set srcdir=..\src
 set pkgdir=%pkgname%-%pkgvers%
 set topdir=..
@@ -19,7 +18,7 @@ set lib64=%pkgdir%\x64
 
 :: miscellanous:
 if not exist %pkgdir% mkdir %pkgdir%
-xcopy /y %libdir%\sample.conf %pkgdir%
+xcopy /y %topdir%\docs\sample.conf %pkgdir%
 xcopy /y %topdir%\README %pkgdir%
 xcopy /y %topdir%\AUTHORS %pkgdir%
 xcopy /y %topdir%\ChangeLog %pkgdir%
@@ -31,6 +30,7 @@ xcopy /y %srcdir%\inifile.h %incdir%
 xcopy /y %srcdir%\cxx\inifile++.hpp %incdir%
 
 :: 32-bit:
+set libdir=vs2008\win32\Release
 if not exist %lib32% mkdir %lib32%
 
 xcopy /y %libdir%\*.dll %lib32%
@@ -38,10 +38,11 @@ xcopy /y %libdir%\*.exp %lib32%
 xcopy /y %libdir%\*.lib %lib32%
 
 :: 64-bit:
+set libdir=vs2008\x64\Release
 if not exist %lib64% mkdir %lib64%
 
 xcopy /y %libdir%\*.dll %lib64%
 xcopy /y %libdir%\*.exp %lib64%
 xcopy /y %libdir%\*.lib %lib64%
 
-"%zipcmd%" a %pkgname%-%pkgvers%.zip %pkgdir%
+"%zipcmd%" a %pkgname%-%pkgvers%-binary.zip %pkgdir%
